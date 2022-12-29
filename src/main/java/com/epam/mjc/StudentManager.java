10f0e73a662e544d4ad377843d6ebfcd8878ae84
stcoderts <115059135+stcoderts@users.noreply.github.com>
2022-12-29 19:41:08 +0500

    just

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,27 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws CustomException {
+
+    if (studentID>=1&&studentID<=10){
+      return Student.getValueOf(studentID);
+    }else {
+      throw new CustomException(
+              "Could not find student with ID "+studentID
+      );
+    }
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
-    }
+      for (int i = 0; i < IDs.length; i++) {
+        try {
+          Student student = manager.find(IDs[i]);
+          System.out.println("Student name " + student.getName());
+        }catch (CustomException ex){
+          System.err.println(ex);
+        }
+      }
 
   }
 }
